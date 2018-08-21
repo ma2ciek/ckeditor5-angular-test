2da2230fffb7f80577ae1b4f565f1e825af03937
Maciej Bukowski <ma2ciek@gmail.com>
2018-08-22 00:54:52 +0200

    Working sample with DecoupledDocument.

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import * as ClassicEditorBuild from '@ckeditor/ckeditor5-build-decoupled-document';
+
+@Component( {
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+} )
+export class AppComponent {
+  title = 'app';
+  foo = 'bar';
+  ClassicEditorBuild = ClassicEditorBuild;
+
+  public model = {};
+
+  public onReady( editor ) {
+    const editableContainer = document.createElement( 'div' );
+    const parent = editor.ui.view.editable.element.parentElement;
+
+    parent.removeChild( editor.ui.view.editable.element );
+    editableContainer.appendChild( editor.ui.view.editable.element );
+
+    parent.appendChild( editor.ui.view.toolbar.element );
+    parent.appendChild( editableContainer );
+  }
+}
