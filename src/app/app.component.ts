2dce8a498671c002b426ce679903b516b422f3f1
Maciej Bukowski <ma2ciek@gmail.com>
2019-04-08 13:50:25 +0200

    Added sample usage of editor with '*ngIf' directive.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,29 @@ import { CKEditor5 } from '@ckeditor/ckeditor5-angular/ckeditor';
 export class AppComponent {
   public ClassicEditorBuild = ClassicEditorBuild;
   public editor: CKEditor5.Editor = null;
+  public editorVisible = false;
 
   public readyEmitter = new EventEmitter<CKEditor5.Editor>();
+
+  public config = {
+    placeholder: 'Geben Sie Ihren Text und formatieren Sie ihn',
+    language: 'de',
+    toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', '|', 'undo', 'redo' ],
+    heading: {
+      options: [
+        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+        { model: 'heading1', view: 'h2', title: 'Heading 1', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h3', title: 'Heading 2', class: 'ck-heading_heading2' },
+      ]
+    }
+  };
 
   public onReady( editor: CKEditor5.Editor ) {
     this.editor = editor;
     this.readyEmitter.emit( this.editor );
+  }
+
+  public toggleEditorVisibility() {
+    this.editorVisible = !this.editorVisible;
   }
 }
