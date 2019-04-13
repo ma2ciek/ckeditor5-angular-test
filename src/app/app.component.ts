f04719d4bae413571ebff28ae2f9b9a99f0119a1
Maciej Bukowski <ma2ciek@gmail.com>
2019-04-13 23:22:19 +0200

    Repro: Integration with angular material tabs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, ViewEncapsulation } from '@angular/core';
 import * as ClassicEditorBuild from '@ckeditor/ckeditor5-build-classic';
 import { CKEditor5 } from '@ckeditor/ckeditor5-angular/ckeditor';
 
 @Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  encapsulation: ViewEncapsulation.None
 } )
 export class AppComponent {
-  public ClassicEditorBuild = ClassicEditorBuild;
+  public Editor = ClassicEditorBuild;
   public editor: CKEditor5.Editor = null;
 
   public readyEmitter = new EventEmitter<CKEditor5.Editor>();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-@NgModule({
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {
+  MatSortModule,
+  MatInputModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatTabsModule
+} from '@angular/material';
+
+@NgModule( {
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    CKEditorModule
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    CKEditorModule,
+    MatTabsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
-})
+  bootstrap: [ AppComponent ]
+} )
 export class AppModule { }
